c6e94c7c8a11c4cb2a47eea08d2970f790a7900c
Rocio Lorca <lorcavillalobos@gmail.com>
2023-07-18 23:02:16 -0400

    perdida

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,28 +1,77 @@
 const analyzer = {  
+
   getWordCount: (text) => {
+    // const sinEspacios = text.trim();
+    // const convertirEnArray = sinEspacios.split(" ");
+    // const contador = convertirEnArray.length;
+    // // console.log(contador)
+    // return contador
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    const sinEspacios= text.trim()
-    const convertirEnArray = sinEspacios.split(" ")   
-    const contador = convertirEnArray.length
-   // console.log(contador);
-   return contador;
+  
+    const word = text.value.trim().split(" ");
+    const contador = word.length;
+    return contador;
   },
+  
   getCharacterCount: (text) => { // parametro
-    
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const character = text.value;
+    return character.length;
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const noSpaces = text.replace(/ /g, "");
+    // const noPunctuation = noSpaces.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    // const characterCount = noPunctuation.length;
+    return noSpaces.length;
+    // console.log(characterCount);
+
+    //o intentar con esta:
+    // let words = text.split(" ");  
+    // let count = 0;  
+    // for (let i = 0; i < words.length; i++) {
+    //   count += words[i].length;  
+    // }  
+    // return count;
   },
+
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(" ");
+    let totalLength = 0;
+  
+    for (let i = 0; i < words.length; i++) {
+      totalLength += words[i].length;  
+    }
+    return totalLength / words.length;  
   },
+
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    let count = 0;
+
+    for (let i = 0; i < text.length; i++) {
+      if (!isNaN(parseInt(text[i]))) {
+        count++;
+      }  
+    }
+    return count;
   },
+
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    let sum = 0;
+
+    for (let i = 0; i < text.length; i++) {   
+      if (!isNaN(parseInt(text[i]))) {
+        sum += parseInt(text[i]); 
+        i += String(parseInt(text[i])).length - 1;
+      }  
+    }
+    return sum;
   },
-};
+}
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -2,27 +2,71 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar los métodos del objeto `analyzer`
 
-const button = document.getElementById('reset-button');
-const textarea = document.querySelector('textarea[name="user-input"]');
+document.querySelector('[data-testid="word-count"]').textContent = "Palabras: 0";
+document.querySelector('[data-testid="character-count"]').textContent = "Caracteres: 0";
+document.querySelector('[data-testid="character-no-spaces-count"]').textContent = "Caracteres sin espacios: 0";
+document.querySelector('[data-testid="number-count"]').textContent = "Numeros: 0";
+document.querySelector('[data-testid="number-sum"]').textContent = "Suma total: 0";
+document.querySelector('[data-testid="word-length-average"]').textContent = "Longitud media de palabras: 0";
 
-button.addEventListener('click', function() {
-  textarea.value = '';
-});
+
+//
+const textarea = document.getElementsByName("user-input")[0];
+// text.addEventListener("keyup", () => {
+textarea.addEventListener('keyup', start) =>{
+  const wordCount = analyzer.getWordCount(text);
+  const charCount = analyzer.getCharacterCount(text);
+  const charNoSpaces = analyzer.getCharacterCountExcludingSpaces(text);
+  const wordLength = analyzer.getAverageWordLength(text);
+  const numCount = analyzer.getNumberCount(text);
+  const numSum = analyzer.getNumberSum(text);
+},
+
+document.querySelector('[data-testing="word-count"]').textContent = "Palabras: " + wordCount;
+document.querySelector('[data-testing="character-count"]').textContent = "Caracteres: " + charCount.toString();
+document.querySelector('[data-testing="character-no-spaces-count"]').textContent = "Caracteres sin espacios: " + charNoSpaces.toString();
+document.querySelector('[data-testing="word-length-average"]').textContent = "Longitud media de palabras: " + wordLength.toString();
+
+  if(numCount !== null){
+    document.querySelector('[data-testing="number-count"]').textContent = "Recuento de números: " + numCount;
+    document.querySelector('[data-testing="number-sum"]').textContent = "Suma total de números: " + numSum;
+  };
+
+
+// function start() {
+//   const wordCount = analyzer.getWordCount(textarea.value);
+//   console.log("Word count:", wordCount);
+// }
+
+// textarea.addEventListener('keyup', start)
+
+
+// const button = document.getElementById('reset-button');
+// const textarea = document.querySelector('textarea[name="user-input"]');
+
+// button.addEventListener('click', function() {
+//   textarea.value = '';
+// });
+
+// const textarea = document.getElementsByName("user-input")[0];
 
 // METRICAS
-function start  (){
-  //  console.log(textarea.value);
-  // objetos y funciones en JS
-  //analyzer.getWordCount // llamar a la funcion
-  analyzer.getWordCount(textarea.value) // invocacion. deuna funcion
-  //console.log(analyzer.getWordCount(textarea.value));
+// document.querySelector('[data-testid="word-count]').textContent = "Palabras: 0";
+
+// function start (){
+//   analyzer.getWordCount(textarea.value);
+//   // console.log(analyzer.getWordCount(textarea.value)) //invocación de la función
+// }
+
+
+
+
+
   // guardar el valor de analyzer.getWordCount(textarea.value) en una variables
   // selector del dom llama al LI word-count
   // innerText o innerHTML + la variable que guarde el valor de analyzer.getWordCount(textarea.value))
 
-}
-textarea.addEventListener('keyup', start );
-
+// console.log(analyzer.getWordCount(textarea.value))
 
 
 
